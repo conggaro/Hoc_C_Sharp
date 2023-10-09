908edde435b44be15ef61e90639b0612ea15852f
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2023-10-09 14:28:52 +0700

    cap nhat

---
+++ Sử dụng generic trong C#/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApp
+{
+    public class Program
+    {
+        // khai báo hàm generic đổi chỗ
+        public static void Doi_Cho<T>(ref T a,ref T b){
+            T temp;
+
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static void Main(string[] args)
+        {
+            int bien1 = 5;
+            int bien2 = 99;
+
+            Console.WriteLine("Ban dau:");
+            Console.WriteLine(bien1);
+            Console.WriteLine(bien2);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Luc sau:");
+            Doi_Cho<int>(ref bien1, ref bien2);
+            Console.WriteLine(bien1);
+            Console.WriteLine(bien2);
+        }
+    }
+}
