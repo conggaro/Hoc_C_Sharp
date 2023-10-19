d84a1b1b00e8e9012b7907d6c7976dcbc317ab39
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2023-10-19 09:28:48 +0700

    cap nhat

---
+++ Chuyển chuỗi string sang kiểu dữ liệu List string/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // cho chuỗi string
+            string str = "abc, def, xyz";
+
+
+            // chuyển chuỗi string
+            // thành danh sách List<string>
+
+            // giải pháp là sử dụng phương thức Split()
+            List<string> ds = str
+                .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+
+            // in ra danh sách
+            Console.WriteLine("DANH SACH CAC PHAN TU STRING:");
+
+
+            foreach (string item in ds)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
