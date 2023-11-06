d249baceadb01c66431eb7af4e3c76b1ad5fb8f8
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2023-11-06 15:47:59 +0700

    cap nhat

---
+++ Gọi thủ tục stored procedure với cả tham số đầu vào và tham số đầu ra/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace MyApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                // tạo biến chứa chuỗi kết nối
+                string connection_str = @"
+                    Server = localhost;
+                    Database = StudentDB;
+                    User ID = sa;
+                    Password = 123456
+                ";
+
+
+                // tạo đối tượng có kiểu "SqlConnection"
+                SqlConnection dt_KetNoi = new SqlConnection(connection_str);
+
+
+                // tạo đối tượng có kiểu "SqlCommand"
+                // để gọi stored procedure trong SQL Server
+                SqlCommand dt_SqlCommand = new SqlCommand()
+                {
+                    // chỉ định tên của stored procedure SQL Server sẽ gọi đến
+                    CommandText = "spCreateStudent",
+
+                    // chỉ định đối tượng kết nối
+                    Connection = dt_KetNoi,
+
+                    // chỉ định loại lệnh là "StoredProcedure"
+                    CommandType = CommandType.StoredProcedure
+                };
+
+
+                /*
+                    mặc định SqlCommand sẽ coi
+                    nội dung trong thuộc tính CommandText
+                    là câu lệnh SQL
+
+                    vì nó đã thiết lập CommandType
+                    bằng CommandType.Text
+
+                    nếu muốn gọi đến Procedure
+                    thì thiết lập nó
+                    bằng CommandType.StoredProcedure
+                */
+
+
+                // tạo đối tượng parameter1
+                // có kiểu dữ liệu "SqlParameter"
+                SqlParameter parameter1 = new SqlParameter
+                {
+                    // cái @Name là tên được định nghĩa trong "stored procedure"
+                    ParameterName = "@Name",
+
+                    // thiết lập kiểu dữ liệu cho tham số @Name
+                    SqlDbType = SqlDbType.NVarChar,
+
+                    // giá trị để truyền cho tham số @Name
+                    Value = "Test",
+
+                    // chỉ định tham số là loại đầu vào (Input)
+                    Direction = ParameterDirection.Input
+                };
+
+
+                // thêm tham số
+                // vào thuộc tính Parameters
+                // của đối tượng SqlCommand
+                dt_SqlCommand.Parameters.Add(parameter1);
+
+
+                // cách 2:
+                // để thêm tham số đầu vào
+                dt_SqlCommand.Parameters.AddWithValue("@Email", "Test@dotnettutorial.net");
+                dt_SqlCommand.Parameters.AddWithValue("@Mobile", "1234567890");
+
+
+                // tạo đối tượng outParameter
+                // có kiểu dữ liệu "SqlParameter"
+                SqlParameter outParameter = new SqlParameter
+                {
+                    // cái @Id là tên được định nghĩa trong "stored procedure"
+                    ParameterName = "@Id",
+
+                    // thiết lập kiểu dữ liệu cho tham số @Id
+                    SqlDbType = SqlDbType.Int,
+
+                    // không cần chỉ định giá trị cho thuộc tính "Value"
+                    // của đối tượng "outParameter"
+
+                    // chỉ định tham số là loại đầu ra (Output)
+                    Direction = ParameterDirection.Output
+                };
+
+
+                // thêm tham số
+                // vào thuộc tính Parameters
+                // của đối tượng SqlCommand
+                dt_SqlCommand.Parameters.Add(outParameter);
+
+
+                // mở kết nối
+                dt_KetNoi.Open();
+
+
+                // gọi phương thức ExecuteNonQuery()
+                dt_SqlCommand.ExecuteNonQuery();
+
+
+                // bây giờ bạn có thể truy cập tham số đầu ra
+                // bằng thuộc tính Value
+                // của đối tượng outParameter
+                Console.WriteLine("ID cua sinh vien moi duoc tao: " + outParameter.Value.ToString());
+
+
+                // đóng chuỗi kết nối
+                dt_KetNoi.Close();
+            }
+            catch (
+                // tạo đối tượng ex
+                // có kiểu dữ liệu "Exception"
+                Exception ex
+            )
+            {
+                // "Exception Occurred" là xảy ra ngoại lệ
+                Console.WriteLine($"Exception Occurred: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Gọi thủ tục stored procedure với cả tham số đầu vào và tham số đầu ra/cơ sở dữ liệu StudentDB.sql
@@ -0,0 +1,75 @@
+﻿-- tạo cơ sở dữ liệu
+CREATE DATABASE StudentDB;
+GO
+
+
+-- sử dụng cơ sở dữ liệu
+USE StudentDB;
+GO
+
+
+-- tạo bảng
+CREATE TABLE Student(
+ [Id] [int] IDENTITY(100,1) PRIMARY KEY,
+ [Name] [varchar](100) NULL,
+ [Email] [varchar](50) NULL,
+ [Mobile] [varchar](50) NULL,
+);
+GO
+
+
+-- thêm dữ liệu vào bảng
+INSERT INTO Student VALUES ('Anurag','Anurag@dotnettutorial.net','1234567890');
+INSERT INTO Student VALUES ('Priyanka','Priyanka@dotnettutorial.net','2233445566');
+INSERT INTO Student VALUES ('Preety','Preety@dotnettutorial.net','6655443322');
+INSERT INTO Student VALUES ('Sambit','Sambit@dotnettutorial.net','9876543210');
+
+
+-- tạo stored procedure
+CREATE PROCEDURE spGetStudents
+AS
+BEGIN
+     SELECT Id, Name, Email, Mobile
+     FROM Student
+END
+
+
+-- tạo stored procedure có truyền tham số
+CREATE PROCEDURE spGetStudentById
+(
+   @Id INT
+)
+AS
+BEGIN
+     SELECT Id, Name, Email, Mobile
+     FROM Student
+     WHERE Id = @Id
+END
+
+
+-- tạo stored procedure
+-- có truyền tham số đầu vào (Input)
+-- và tham số đầu ra (Output)
+CREATE PROCEDURE spCreateStudent
+(
+    @Name VARCHAR(100),
+    @Email VARCHAR(50),
+    @Mobile VARCHAR(50),
+    @Id int Out
+)
+AS
+BEGIN
+	 -- chèn dữ liệu vào bảng
+     INSERT INTO Student VALUES (@Name,@Email,@Mobile)
+
+
+	 -- điều chúng ta muốn ở đây
+	 -- là chúng ta cần trả về Id sinh viên
+	 -- mới được tạo
+	 -- và đây là lúc tham số đầu ra xuất hiện
+
+	 -- ở đây, chúng ta đặt giá trị
+	 -- tham số đầu ra với Id sinh viên mới được tạo
+	 -- bằng cách sử dụng hàm SCOPE_IDENTITY()
+     SELECT @Id = SCOPE_IDENTITY()
+END
