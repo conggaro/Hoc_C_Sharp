6e32aa64a06d399fbf0579885c5ffed367876c77
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2023-12-20 12:09:54 +0700

    cap nhat

---
+++ cách sử dụng toán tử null coalescing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // cách sử dụng toán tử null coalescing ??
+            // để gán giá trị mặc định là 10
+            // cho biến x
+            // nếu giá trị của x là null
+
+            int? x = null;
+            Console.WriteLine(x);
+
+            x = x ?? 10;
+            Console.WriteLine(x);
+
+
+            // nó tương đương với câu lệnh
+            //if (x == null)
+            //{
+            //    x = 10;
+            //}
+            //Console.WriteLine(x);
+        }
+    }
+}
