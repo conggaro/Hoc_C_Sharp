b84b451adaa2a317ff73d3e8542fba2f3edf1503
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2024-02-22 11:44:45 +0700

    cap nhat

---
+++ sử dụng lớp Span _ dịch ra là lớp nhịp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3 };
+
+
+            // sử dụng lớp "Span"
+            // Span dịch ra là "nhịp"
+            Span<int> spans = new Span<int>(arr);
+
+
+            // in các phần tử
+            // trong đối tượng spans
+            // ra màn hình
+            for (int i = 0; i < spans.Length; i++)
+            {
+                Console.WriteLine(spans[i]);
+            }
+        }
+    }
+}
---
+++ toán tử trải rộng _  spread operator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr1 = new int[] { 1, 2, 3 };
+            int[] arr2 = new int[] { 4, 5, 6 };
+            int[] arr3 = new int[] { 7, 8, 9 };
+
+
+            // sử dụng toán tử trải rộng ".."
+            // tiếng Anh là "Spread operator"
+            int[] single = [..arr1,..arr2,..arr3];
+
+
+            // in ra các phần tử trong mảng single
+            foreach (int item in single)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
