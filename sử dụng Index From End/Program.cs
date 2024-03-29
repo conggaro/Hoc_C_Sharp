9b18f8cb873a0c82a84d5562ad6eaf1f94084d57
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2024-03-29 10:19:20 +0700

    cap nhat

---
+++ sử dụng Index From End/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+/*
+    Trong đoạn mã của bạn,
+    string temp1 = str[..6]
+    là một phần của cú pháp mới
+    được giới thiệu trong C# 8.0 gọi là "Index from End" (chỉ số từ cuối).
+    
+    Điều này cho phép bạn truy cập
+    các phần tử của một chuỗi từ cuối của chuỗi đó.
+
+    Điều này là một cách ngắn gọn
+    và tiện lợi hơn để cắt chuỗi từ cuối
+    về đầu mà không cần phải tính toán vị trí index thủ công.
+*/
+
+namespace MyApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str = "abcdef123456";
+
+            
+            // khi bạn chỉ muốn lấy 6 ký tự
+            // tính từ đầu
+            string temp1 = str[..6];
+
+
+            // khi bạn muốn lấy 10 ký tự
+            // tính từ đầu
+            // thì bạn phải trừ đi 2 ký tự cuối
+            string temp2 = str[..(str.Length - 2)];
+
+
+            Console.WriteLine(str + "\n");
+            Console.WriteLine(temp1 + "\n");
+            Console.WriteLine(temp2);
+        }
+    }
+}
---
+++ truyền tham số là string vào trong Where/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+
+/*
+    bạn phải cài đặt thư viện
+    vào trong NuGet Package
+
+    System.Linq.Dynamic.Core
+    System.Linq
+    System.Collections.Generic
+*/
+
+namespace MyApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // tạo danh sách
+            List<string> words = new List<string>
+            {
+                "apple",
+                "banana",
+                "cherry",
+                "date",
+                "elderberry"
+            };
+
+
+            // biểu thức lambda dưới dạng chuỗi
+            // nó cũng là biểu thức điều kiện luôn
+            string condition = "x => x.Length > 5";
+
+            // Sử dụng phương thức Where để lọc các phần tử dựa trên điều kiện
+            var result = words.AsQueryable().Where(condition);
+
+
+            // cách 2:
+            // var result2 = words.AsQueryable().Where("x => x.Length > @0", 5);
+
+
+            // In ra các phần tử đã được lọc
+            foreach (var word in result)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
