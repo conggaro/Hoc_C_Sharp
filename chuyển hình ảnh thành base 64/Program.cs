171f1fb9d52be7ab67c589c60710ef97f3520eb0
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2025-01-13 08:53:12 +0700

    cap nhat

---
+++ chuyển hình ảnh thành base 64/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string imagePath = "C:\\Users\\CongPC\\Music\\Flag_of_Vietnam.svg.png"; // Thay đổi đường dẫn tới hình ảnh của bạn
+        string base64String = ConvertImageToBase64(imagePath);
+
+        Console.WriteLine(base64String);
+
+        // nếu bạn muốn ghi file
+        // File.WriteAllText(@"C:\Users\CongPC\Videos\file_out.txt", base64String);
+    }
+
+    static string ConvertImageToBase64(string imagePath)
+    {
+        byte[] imageBytes = File.ReadAllBytes(imagePath);
+        return Convert.ToBase64String(imageBytes);
+    }
+}
---
+++ chuyển string base 64 thành hình ảnh/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string filePathSource = "C:\\Users\\CongPC\\Music\\vjp.txt";
+
+        // Đọc toàn bộ nội dung file
+        string content = File.ReadAllText(filePathSource);
+
+        string base64String = content;
+
+        string filePath = "C:\\Users\\CongPC\\Videos\\file.png"; // Đường dẫn nơi bạn muốn lưu tệp hình ảnh
+
+        ConvertBase64ToFile(base64String, filePath);
+        Console.WriteLine("Tệp hình ảnh đã được lưu thành công!");
+    }
+
+    static void ConvertBase64ToFile(string base64String, string filePath)
+    {
+        // Giải mã chuỗi Base64 thành mảng byte
+        byte[] fileBytes = Convert.FromBase64String(base64String);
+
+        // Ghi mảng byte vào tệp
+        File.WriteAllBytes(filePath, fileBytes);
+    }
+}
